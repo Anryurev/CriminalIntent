17d8987b9954e378ab8e648f4583c40e7e28bd81
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-12 23:51:16 +0700

    Начало ViewHolder (CrimeListFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -33,6 +33,9 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         return view
     }
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+    }
 
 
     companion object {
