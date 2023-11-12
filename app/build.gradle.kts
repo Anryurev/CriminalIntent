4ce933a7dfe2fdc5238da2cfdd82ea19cc48dfb4
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-12 23:20:44 +0700

    Создание макета контейнера фрагмента (activity_crime.xml = activity_main.xml)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.android.criminalintent"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bignerdranch.android.criminalintent"
