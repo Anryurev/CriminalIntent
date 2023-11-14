aa8970462202c89478fc61a9047bec885c6b9391
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-15 00:19:52 +0700

    Добавление функций TypeConverter (database/CrimeTypeConverters.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
@@ -4,7 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.bignerdranch.android.criminalintent.Crime
 
-@Database(entities = [ Crime::class ],
-    version=1)
+@Database(entities = [ Crime::class ], version=1)
 abstract class CrimeDatabase : RoomDatabase() {
 }
