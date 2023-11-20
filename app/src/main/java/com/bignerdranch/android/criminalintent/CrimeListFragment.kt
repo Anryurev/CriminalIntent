75db94119672b24acbf290f6e89634dc0fa5c103
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-20 21:05:31 +0700

    Добавление интерфейса обратного вызова (CrimeListFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,15 +13,30 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.UUID
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
+
+    /**
+     * Требуемый интерфейс
+     */
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+    private var callbacks: Callbacks? = null
 
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +61,12 @@ class CrimeListFragment : Fragment() {
                 }
             })
     }
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
 
     private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
