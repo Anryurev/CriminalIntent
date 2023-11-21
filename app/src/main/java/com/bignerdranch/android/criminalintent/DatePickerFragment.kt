9e999b68ed33cf963ccda3c241089d6a75f33fdd
Anryurev <137984074+Anryurev@users.noreply.github.com>
2023-11-21 11:53:05 +0700

    Добавление dateListener (DatePickerFragment.kt)

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -23,14 +23,13 @@ class DatePickerFragment : DialogFragment() {
                     (fragment as Callbacks).onDateSelected(resultDate)
                 }
             }
-
         val date = arguments?.getSerializable(ARG_DATE) as Date
         val calendar = Calendar.getInstance()
         calendar.time = date
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
-        return DatePickerDialog(requireContext(), null, initialYear, initialMonth, initialDay)
+        return DatePickerDialog(requireContext(), dateListener, initialYear, initialMonth, initialDay)
     }
 
     companion object {
